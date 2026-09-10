663e4a66c98dc4e8bdf4efd1c454d4e67580e874
Emre <emre.bertan@arkas.com>
2026-09-10 17:24:31 +0300

    Include firebase-config.js so GitHub Pages can load it

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Gerçek Firebase bilgileriniz burada; bu dosya .gitignore ile GitHub'a gönderilmez.
+export const firebaseConfig = {
+    apiKey: "AIzaSyAJyCraWkG-C2V6e-MngXJhvkBSbTinmO8",
+    authDomain: "avrupa-tahmin-2026.firebaseapp.com",
+    projectId: "avrupa-tahmin-2026",
+    storageBucket: "avrupa-tahmin-2026.firebasestorage.app",
+    messagingSenderId: "941314403600",
+    appId: "1:941314403600:web:00169f3752189db7b8c995",
+    measurementId: "G-ZCNP1QX6RG"
+};
